6a427146cc715018a6d1d54955540c16e48cf76f
salman-281 <salman034810@gmail.com>
2025-08-05 22:33:07 -0700

    check in

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-//cors
+//cors this is used to allow the frontend to access the backend
 const cors = require('cors');
 
 
